d5b0a780d4a573ab157fd1a489b8c1f4b93cbd45
boonmee <boonmeepuk2@gmail.com>
2018-06-27 16:22:45 +0700

    Finish 27June

--- app/src/main/java/masterung/androidthai/in/th/srrufriend/MainActivity.java
+++ app/src/main/java/masterung/androidthai/in/th/srrufriend/MainActivity.java
@@ -3,11 +3,22 @@ package masterung.androidthai.in.th.srrufriend;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import masterung.androidthai.in.th.srrufriend.fragment.mainfragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        // Add Fragment to Activity
+        if(savedInstanceState == null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragment, new mainfragment())
+                    .commit();
+        }
+
+    }// Main method
+
+}// Main Class
